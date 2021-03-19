0feeb98ee31843c5f469e2713ee1b64a57f228d4
Thuy MAI <thuydung.mai@celad.com>
2021-03-19 14:12:50 +0100

    add: docker compose

---
+++ mysql/init.sql
@@ -0,0 +1,23 @@
+-- Host: localhost    Database: registrationdb
+-- ------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS registrationdb ;
+
+USE registrationdb;
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL UNIQUE,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) DEFAULT '0X00',
+  `famillyname` varchar(255) DEFAULT '0X00',
+  `address` varchar(255) DEFAULT '0X00',
+  `phone` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
